1e2fca13025ea775e928d0bde1e3ba0723c1172e
jayalemao <jayalemao@2ea685c1-1f4d-0410-943b-67b15c07319a>
2008-09-29 21:44:49 +0000

    Implantado efeito Fadein e FadeOut nos menus. Ainda faltam os menus de botão direito. Alterada a página trocar o background, agora é soó clocar no radio button.

--- trunk/RichBlocks/libs/interface/common.js
+++ trunk/RichBlocks/libs/interface/common.js
@@ -237,6 +237,35 @@ function cancelEvent(event){
 	    }
 }
 
-function loadScripts(scriptId){
-	alert(scriptId);
+
+
+function fadeOut(id, time) {
+	target = document.getElementById(id);
+	alpha = 100;
+	timer = (time*1000)/50;
+	var i = setInterval(
+			function() {
+				if (alpha <= 0)
+					clearInterval(i);
+				setAlpha(target, alpha);
+				alpha -= 2;
+			}, timer);
+}
+
+function fadeIn(id, time) {
+	target = document.getElementById(id);
+	alpha = 0;
+	timer = (time*1000)/50;
+	var i = setInterval(
+			function() {
+				if (alpha >= 100)
+					clearInterval(i);
+				setAlpha(target, alpha);
+				alpha += 2;
+			}, timer);
+}
+
+function setAlpha(target, alpha) {
+	target.style.filter = "alpha(opacity="+ alpha +")";
+	target.style.opacity = alpha/100;
 }
